92b160083cf2b1c21c8428166452bfe84fca5671
mykaya24 <78302635+mykaya24@users.noreply.github.com>
2026-01-28 14:41:06 +0300

    view engine is completed

--- app.js
+++ app.js
@@ -1,13 +1,25 @@
 const express = require('express');
+const ejs = require('ejs');
 const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(express.static('public'));
 
 const port = 3000;
 
-app.get('/',(req,res)=>{
-    res.send('{ id: 1, title: "Blog title", description: "Blog description" }');
-})
+app.get('/', (req, res) => {
+  res.render('index');
+});
 
+app.get('/about', (req, res) => {
+  res.render('about');
+});
 
-app.listen(port,()=>{
-    console.log("Sunucu Hazır");
-})
+app.get('/addPost', (req, res) => {
+    res.render('add_post');
+  });
+
+app.listen(port, () => {
+  console.log('Sunucu Hazır');
+});
